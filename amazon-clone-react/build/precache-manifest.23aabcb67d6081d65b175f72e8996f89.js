d0d179263a96ce5d66270f3bc5ee95f61d158193
nababsk123 <124558435+nababsk123@users.noreply.github.com>
2023-05-17 11:31:54 +0530

    ECOMMERCE CLONE

---
+++ amazon-clone-react/build/precache-manifest.23aabcb67d6081d65b175f72e8996f89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe3ebbf56dedd19d5a677296fc7b55f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21e16f97ba22f42ac193",
+    "url": "/static/css/main.a81ea7a8.chunk.css"
+  },
+  {
+    "revision": "ff287c9125369a90eb15",
+    "url": "/static/js/2.683bb502.chunk.js"
+  },
+  {
+    "revision": "9cf9d893b5c51de0aa6dd9e07be64537",
+    "url": "/static/js/2.683bb502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21e16f97ba22f42ac193",
+    "url": "/static/js/main.934c98d4.chunk.js"
+  },
+  {
+    "revision": "b3746bd0eb65763f9cdc",
+    "url": "/static/js/runtime-main.ca2ded2f.js"
+  }
+]);
